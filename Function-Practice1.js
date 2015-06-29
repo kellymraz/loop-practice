1b17948ce61d8e724f1d950844caa6be8b16ebd6
Student <kitty8mars@gmail.com>
2015-06-29 17:16:19 -0600

    Jason will help me

---
+++ Function-Practice1.js
@@ -0,0 +1,30 @@
+// for (var i = 1; i <= 3; i++)
+// 	console.log("Five!")
+
+// Problem 2
+
+
+// 	var singleString = prompt("Please type in one word");
+// 		console.log(singleString);
+
+
+// Find the length property of the string 
+// CharAt or use bracket notation to index directly into 
+
+
+// 		var lastLetter = function(){
+// 			singleString
+
+// 		}
+
+// 		str.slice(-1)
+
+// return 
+
+// Problem 3
+
+var numberArgument = prompt("Please type in a number");
+		console.log(numberArgument);
+
+
+
