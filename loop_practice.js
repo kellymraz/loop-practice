743053695c0a30dfd67b3fbd7b7ae9a1ddb4f536
Student <kitty8mars@gmail.com>
2015-06-29 15:48:20 -0600

    did this one again

--- loop_practice.js
+++ loop_practice.js
@@ -212,19 +212,53 @@
 // 	console.log(i);
 // }
 
-for (var i = 0; i <= 3; i++) {
+
+// Loop Practice 1
+// Problem 1
+
+// for(var i = 0; i <= 500; i = i + 100){
+// 	console.log(i);
+// }
+
+// Problem 2
+// for (var i = 1; i <=64; i = i*2){
+// 	console.log(i)
+// }
+
+// Problem 3
+
+// for (var i = 1; )
+
+// Problem 4
+
+// for (var i = 1; i <= 3; i++){
+// 	for (var j = 1; j <= 3; j++){
+// 		console.log(i, j)
+// 		}
+// }
+
+// Problem 5
+
+// for (var i = 0; i <= 10; i = i + 2){
+// 	console.log(i)
+// }
+
+// Problem 6
+
+// for (var i = 3; i <= 15; i = i + 3){
+// 	console.log(i)
+// }
+
+// for (var i = 9; i >= 0; i--)
+// 	console.log(i)
+
+for (var i = 0; i <= 3; i++){
 	for (var j = 0; j <= 3; j++){
 		console.log(j)
+
+
 	}
 }
-
-
-
-
-
-
-
-
 
 
 
